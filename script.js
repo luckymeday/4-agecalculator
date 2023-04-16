8d972cc3d1c37002431139e0b70037b7dcf8a959
jeesunlee <luckymeday@gmail.com>
2023-04-16 13:08:41 -0700

    color change

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ function age() {
   var d1 = document.getElementById("dayInput").value;
   var m1 = document.getElementById("monthInput").value;
   var y1 = document.getElementById("yearInput").value;
-  // console.log("input " + d1, m1, y1);
+  console.log("input " + d1, m1, y1);
   var date = new Date(); // -> pulling today date
-  // console.log("pulling today " + date);
+  console.log("pulling today " + date);
 
   // -> setting today' date def.
   var d2 = date.getDate();
   var m2 = 1 + date.getMonth();
   var y2 = date.getFullYear();
-  // console.log("today " + d2, m2, y2);
+  console.log("today " + d2, m2, y2);
   var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
   if (d1 > d2) {
@@ -19,7 +19,10 @@ function age() {
     m2 = m2 - 1;
   } else if (d1 > 31) {
     console.log("Must be a valid day");
-    document.getElementById("alertDay").innerHTML = "Must be a valid day";
+    document.getElementById("dayInput").style.borderColor = "red";
+    document.getElementById("titleDay").style.color = "red";
+    document.getElementById("alertDay").innerHTML =
+      "<span style='color:red;'> Must be a valid day</span>";
     // document.getElementById("age").innerHTML = "--";
   }
 
@@ -29,13 +32,19 @@ function age() {
   } else if (m1 > 13);
   {
     console.log("Must be a valid month");
-    document.getElementById("alertMonth").innerHTML = "Must be a valid month";
+    document.getElementById("monthInput").style.borderColor = "red";
+    document.getElementById("titleMonth").style.color = "red";
+    document.getElementById("alertMonth").innerHTML =
+      "<span style='color:red;'> Must be a valid month</span>";
     // document.getElementById("age").innerHTML = "--";
   }
 
   if (y1 > y2) {
     console.log("Must be in the past");
-    document.getElementById("alertYear").innerHTML = "Must be in the past";
+    document.getElementById("yearInput").style.borderColor = "red";
+    document.getElementById("titleYear").style.color = "red";
+    document.getElementById("alertYear").innerHTML =
+      "<span style='color:red;'>Must be in the past</span>";
     // document.getElementById("age").innerHTML = "--";
   }
 
@@ -45,10 +54,11 @@ function age() {
 
   document.getElementById("age").innerHTML =
     y +
-    "<span style='font-size 32px ; font-style:italic ;'> Years </span>" +
+    "<span style='font-size 32px ; font-style:italic:bold ;'> Years </span>" +
     "<br />" +
     m +
-    "<span style='font-size 32px ; font-style:italic ;'> Months </span>" +
-    "<br />";
-  d + "<span style='font-size 32px ; font-style:italic ;'> Days </span>";
+    "<span style='font-size 32px ; font-style:italic:bold ;'> Months </span>" +
+    "<br />" +
+    d +
+    "<span style='font-size 32px ; font-style:italic:bold ;'> Days </span>";
 }
